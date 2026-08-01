ea2fa097e2cf21cd7e24960778f55406827b05ff
Matthew Li <matthewli.mtl@gmail.com>
2026-08-01 14:50:37 -0700

    base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  base: '/tutoring-website/',
   build: {
     // Highlight-start
     outDir: 'dist', // Forces the folder to build directly at your repository root
